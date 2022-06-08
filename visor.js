92a865eb88854f452a2cb0cddfbe29a0ae3fb344
Coni Luciani <Omeguis@users.noreply.github.com>
2022-06-08 18:34:46 -0300

    color pickers

--- visor.js
+++ visor.js
@@ -28,3 +28,40 @@ setInterval(function() {
     totalkillsLocal !== null ? percentagewrapper.innerHTML = winpercentageLocal + "%" : percentagewrapper.innerHTML = "N/A";
 
 }, 6000); 
+
+
+//Eventos de Btns
+
+const colorPickerSelectBtop = document.getElementById("colorpickerbtop");
+const colorPickerSelectBbottom = document.getElementById("colorpickerbbottom");
+const colorPickerSelectBgrid = document.getElementById("colorpickergrid");
+
+colorPickerSelectBtop.addEventListener("input", selectColorBtop);
+colorPickerSelectBbottom.addEventListener("input", selectColorBbottom);
+colorPickerSelectBgrid.addEventListener("input", selectColorGrid);
+
+let borderTopElement = document.getElementById('supBorder');
+let borderBottomElement = document.getElementById('bottomBorder');
+let borderBottomElementO = document.getElementById('first-cell');
+let borderBottomElementT = document.getElementById('second-cell');
+let borderBottomElementTh = document.getElementById('third-cell');
+
+function selectColorBtop(){
+    let selectedColor = colorPickerSelectBtop.value;
+    borderTopElement.style.cssText = `background-color: ${selectedColor}`;
+}
+
+function selectColorBbottom(){
+    let selectedColor = colorPickerSelectBbottom.value;
+    borderBottomElement.style.cssText = `background-color: ${selectedColor}`;
+}
+
+function selectColorGrid(){
+    let selectedColor = colorPickerSelectBgrid.value;
+    borderBottomElementO.style.cssText = `background-color: ${selectedColor}`;
+    borderBottomElementT.style.cssText = `background-color: ${selectedColor}`;
+    borderBottomElementTh.style.cssText = `background-color: ${selectedColor}`;
+}
+
+
+
